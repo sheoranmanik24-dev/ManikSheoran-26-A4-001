19cef1a653d61a650dd4403f81f584639673343c
sheoranmanik24-dev <sheoranmanik24@gmail.com>
2026-08-27 02:03:45 +0530

    grayscale

--- TASK1.py
+++ TASK1.py
@@ -1,4 +1,8 @@
 import cv2
+import numpy as np
 image = cv2.imread('2.png')
-cv2.imshow('result',image)
-cv2.waitkey(0) 
+laneimage= np.copy(image)
+gray=cv2.cvtColor(laneimage, cv2.COLOR_BGR2GRAY)
+cv2.imshow('result',gray)
+cv2.waitKey(0) 
+
